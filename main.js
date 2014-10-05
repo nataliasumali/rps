26cb775630df809edad7bd13fa789a2f8f6d4b67
nataliaSumali <natalia.sumali@gmail.com>
2014-10-04 18:46:01 -0700

    get the game working

--- main.js
+++ main.js
@@ -1,36 +1,44 @@
-var userChoice;
+var user;
+    var choose = function(choice) {
+        user = choice;
+    }
 
-var computerChoice = Math.random();
-if (computerChoice < 0.34) {
-	computerChoice = "rock";
-} else if(computerChoice <= 0.67) {
-	computerChoice = "paper";
-} else {
-	computerChoice = "scissors";
-} console.log("Computer: " + computerChoice);
+    var computerChoice = Math.random();
+        if (computerChoice < 0.34) {
+        computerChoice = "rock";
+        } 
+        else if(computerChoice < 0.68) {
+            computerChoice = "paper";
+        } 
+        else {
+        computerChoice = "scissors";
+        } 
 
-var compare = function(choice1, choice2) { 
+    var compare = function(choice1, choice2) { 
         if (choice1 === choice2) {
             document.getElementById("result").innerHTML = "Tie!";
-    } else if (choice1 === "rock") {
-    if (choice2 === "scissors") {
-            document.getElementById("result").innerHTML = "You Win!"
-    } else {
-            document.getElementById("result").innerHTML = "You Lose!";
+        } 
+        else if (choice1 === "rock") {
+            if (choice2 === "scissors") {
+                document.getElementById("result").innerHTML = "You Win!";
+            } 
+            else {
+                document.getElementById("result").innerHTML = "You Lose!";
+            }
+        } 
+        else if (choice1 === "paper") {
+            if (choice2 === "rock") {
+                document.getElementById("result").innerHTML = "You Win!";
+            } 
+            else {
+                document.getElementById("result").innerHTML = "You Lose!";
+            }
+        } 
+        else if (choice1 === "scissors") {
+            if (choice2 === "rock") {
+                document.getElementById("result").innerHTML = "You Lose!";
+            } else {
+                document.getElementById("result").innerHTML = "You Win!";
+            }
+        }
     }
-    } else if (choice1 === "paper") {
-    if (choice2 === "rock") {
-            document.getElementById("result").innerHTML = "You Win!"
-    } else {
-            document.getElementById("result").innerHTML = "You Lose!";
-    }
-    } else if (choice1 === "scissors") {
-    if (choice2 === "rock") {
-            document.getElementById("result").innerHTML = "You Win!"
-    } else {
-            document.getElementById("result").innerHTML = "You Lose!";
-    }
-    }
-    }
-
-compare(userChoice, computerChoice)
